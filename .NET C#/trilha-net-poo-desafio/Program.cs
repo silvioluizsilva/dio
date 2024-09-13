2e0d3515963c2ba607f9de412820bb4ef3dc8494
silvioluizsilva <silvioluizsilva@gmail.com>
2024-09-13 20:44:04 -0300

    Adicionando Trilha .NET Desafio POO

---
+++ .NET C#/trilha-net-poo-desafio/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DesafioPOO.Models;
+
+
+// Smartphone i = new Iphone();
+// Smartphone n = new Nokia();
+
+
+Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new Nokia("123456", "Modelo 1", "1111111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone");
+Iphone iphone = new Iphone("987654", "Modelo 2", "2222222222", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
